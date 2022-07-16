eae61ef29f5f1cb24d1de04ba58199a0840af7a8
RizalAchp <rizal.ahmadp@gmail.com>
2022-07-17 03:52:38 +0700

    change make command with cmake,causes brake in windows

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -61,7 +61,7 @@ return require("packer").startup {
 
     use {
       "nvim-telescope/telescope-fzf-native.nvim",
-      run = "make"
+      run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
     }
 
     use {
